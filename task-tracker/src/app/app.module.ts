174f822a8ab41348df4dc3a05f7a19b722870a6d
mcube25 <mcube3567@gmail.com>
2021-07-01 00:51:44 -0700

    fontawesome

--- task-tracker/src/app/app.module.ts
+++ task-tracker/src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { ButtonComponent } from './components/button/button.component';
 import { TasksComponent } from './components/tasks/tasks.component';
-import { TaskItemComponent } from './component/task-item/task-item.component';
+import { TaskItemComponent } from './components/task-item/task-item.component';
 
 @NgModule({
   declarations: [
