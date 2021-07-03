3143f8ec92da7fc032a2384f591c42ea583b1770
mcube25 <mcube3567@gmail.com>
2021-07-02 18:21:26 -0700

    added data base

--- task-tracker/src/app/services/task.service.ts
+++ task-tracker/src/app/services/task.service.ts
@@ -1,16 +1,16 @@
 import { Injectable } from '@angular/core';
-import { TASKS } from '../mock-tasks';
-import { Observable, of } from 'rxjs';
+import { Observable} from 'rxjs';
 import {Task} from '../Task';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
+  private apiUrl = 'http://localhost:5000/tasks'
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
   getTask(): Observable<Task[]> {
-    const tasks = of(TASKS);
-    return tasks;
+    return this.http.get<Task[]>(this.apiUrl);
   }
 }
